ed2cdcd08d1d413cefca83725ce1a3952154ed5f
Yang <yangjingren@gmail.com>
2014-03-25 16:47:04 -0400

    hi

--- shuffle.c
+++ shuffle.c
@@ -3,17 +3,15 @@ void* shuffle_i(void *tn){
     struct list_node* tmp =temp[id];
     while(tmp->next!=NULL)
     {
-        node *tempint = new node;
+        
         tempint -> next = NULL;
         tempint -> key =tmp->value;
         tempint ->value = tmp ->key;
         
-        // Check If There Is An Initial Record In the List
         if (rootint != NULL)
         {
             current = rootint;
             
-            // temporary pointers location
             if ( tempint -> value < rootint -> value )
             {
                 tempint -> next = current;
@@ -26,7 +24,6 @@ void* shuffle_i(void *tn){
                 
                 while ( (tempint -> value > current -> value) && ( current -> next != NULL ) )
                 {
-                    // Set the insert location to the current location then change the current location to the next position
                     InsertPointer = current;
                     current = current -> next;
                     
@@ -41,7 +38,6 @@ void* shuffle_i(void *tn){
                 }
             }
         }
-        // If there is no initial record assign the new record to the Root position
         else 
         {
             rootint = tempint;
