76de32c2400c7411262611859d8a96c221798bed
Dan <danielsu6401@gmail.com>
2014-03-25 17:20:21 -0400

    value -> count

--- shuffle.c
+++ shuffle.c
@@ -1,48 +1,5 @@
 void* shuffle_i(void *tn){
     int id =(intptr_t) tn;
     struct list_node* tmp =temp[id];
-    while(tmp->next!=NULL)
-    {
-        
-        tempint -> next = NULL;
-        tempint -> key =tmp->value;
-        tempint ->value = tmp ->key;
-        
-        if (rootint != NULL)
-        {
-            current = rootint;
-            
-            if ( tempint -> value < rootint -> value )
-            {
-                tempint -> next = current;
-                rootint = tempint;
-            }
-            
-            else if ( tempint -> value > root -> value )
-            {
-                list_node *InsertPointer = new node;
-                
-                while ( (tempint -> value > current -> value) && ( current -> next != NULL ) )
-                {
-                    InsertPointer = current;
-                    current = current -> next;
-                    
-                }
-                
-                if ( (tempint -> value > current -> value) && ( current -> next == NULL ) )
-                    current -> next = tempint;
-                else
-                {
-                    InsertPointer -> next = temp;
-                    temp -> next = current;
-                }
-            }
-        }
-        else 
-        {
-            rootint = tempint;
-        }
-        
-    }
-}
+    
 }
